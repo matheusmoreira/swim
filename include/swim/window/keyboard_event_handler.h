e68dee5b1175810fffc3b98493066baafd15a0ba
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-03-18 16:24:37 -0300

    Make mouse and keyboard event handlers take a swim_window pointer

--- include/swim/window/keyboard_event_handler.h
+++ include/swim/window/keyboard_event_handler.h
@@ -8,6 +8,8 @@ typedef struct swim_window swim_window;
 /**
  * Pointer to a function that handles the mouse events of a window.
  */
-typedef void (*swim_window_keyboard_event_handler)(swim_keyboard_key key, void * context);
+typedef void (*swim_window_keyboard_event_handler)(swim_window * window,
+                                                   swim_keyboard_key key,
+                                                   void * context);
 
 #endif /* SWIM_WINDOW_KEYBOARD_EVENT_HANDLER_H */
--- include/swim/window/mouse_event_handler.h
+++ include/swim/window/mouse_event_handler.h
@@ -8,6 +8,8 @@ typedef struct swim_window swim_window;
 /**
  * Pointer to a function that handles the keyboard events of a window.
  */
-typedef void (*swim_window_mouse_event_handler)(swim_mouse_button button, void * context);
+typedef void (*swim_window_mouse_event_handler)(swim_window * window,
+                                                swim_mouse_button button,
+                                                void * context);
 
 #endif /* SWIM_WINDOW_MOUSE_EVENT_HANDLER_H */
